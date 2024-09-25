cf689230fe083aae8e3b90b013f380d64e0973e8
Yusuke Shinohara <complex.alpha.cxa@gmail.com>
2024-09-25 11:06:44 +0900

    キーで動く

--- player.gd
+++ player.gd
@@ -9,4 +9,20 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y += -1
+	if Input.is_action_pressed("move_left"):
+		velocity.x += -1
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+		
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$PlayerAnime.play()
+	else:
+		$PlayerAnime.stop()
+		
+	
