b33552557c69ba226562b3fab9270ff318fddf19
Yusuke Shinohara <complex.alpha.cxa@gmail.com>
2024-09-25 16:29:12 +0900

    enemyランダムタイプ

---
+++ enemy.gd
@@ -0,0 +1,12 @@
+extends RigidBody2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var enemy_types = $EnemyAnimatedSprite2D.sprite_frames.get_animation_names
+	$EnemyAnimatedSprite2D.play(enemy_types[randi() % enemy_types.size()])
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
